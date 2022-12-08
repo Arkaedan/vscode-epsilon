e824c8e936dee93ca0d00b90edd4c2124c041df7
Sam Harris <sam.harris@codebots.com>
2022-12-08 15:12:19 +1000

    Convert indentation to tabs

--- src/extension.ts
+++ src/extension.ts
@@ -7,53 +7,53 @@ export function activate(context: vscode.ExtensionContext) {
 
 	vscode.window.registerTerminalLinkProvider({
 		provideTerminalLinks: (context, token) => {
-            
-            // Regular expression that matches Epsilon program locations
-            let regexp = /\(((.*?)@(\d*):(\d*)-(\d*):(\d*))\)/i
+			
+			// Regular expression that matches Epsilon program locations
+			let regexp = /\(((.*?)@(\d*):(\d*)-(\d*):(\d*))\)/i
 
-            // Match the line against the regexp
-            let matches = context.line.match(regexp);
+			// Match the line against the regexp
+			let matches = context.line.match(regexp);
 
-            if (matches != null) {
-                let startIndex = context.line.indexOf(matches[1]);
-                let length = matches[1].length;
-                let data = {
-                    file: matches[2], 
-                    startLine: parseInt(matches[3]),
-                    startColumn: parseInt(matches[4]),
-                    endLine: parseInt(matches[5]),
-                    endColumn: parseInt(matches[6])
-                };
-                return [
-                    {
-                        startIndex,
-                        length: length,
-                        data: data
-                    }
-			    ];
-            }
-            else {
-                return [];
-            }
+			if (matches != null) {
+				let startIndex = context.line.indexOf(matches[1]);
+				let length = matches[1].length;
+				let data = {
+					file: matches[2], 
+					startLine: parseInt(matches[3]),
+					startColumn: parseInt(matches[4]),
+					endLine: parseInt(matches[5]),
+					endColumn: parseInt(matches[6])
+				};
+				return [
+					{
+						startIndex,
+						length: length,
+						data: data
+					}
+				];
+			}
+			else {
+				return [];
+			}
 		},
 		handleTerminalLink: (link: any) => {
 
-            // Open an editor for the file
-            vscode.workspace.openTextDocument(link.data.file).then(document => 
-                // Show the editor
-                vscode.window.showTextDocument(document)).then(x => {
-                    let activeEditor = vscode.window.activeTextEditor;
-                    if (activeEditor != null) {
-                        // Find the range to reveal
-                        let range = new vscode.Range(new vscode.Position(link.data.startLine - 1, link.data.startColumn), new vscode.Position(link.data.endLine - 1, link.data.endColumn));
-                        // Reveal the range
-                        activeEditor.selection = new vscode.Selection(range.start, range.end);
-                        activeEditor.revealRange(range);
-                    }
-                });
-		    },
-	    }
-    );
+			// Open an editor for the file
+			vscode.workspace.openTextDocument(link.data.file).then(document => 
+				// Show the editor
+				vscode.window.showTextDocument(document)).then(x => {
+					let activeEditor = vscode.window.activeTextEditor;
+					if (activeEditor != null) {
+						// Find the range to reveal
+						let range = new vscode.Range(new vscode.Position(link.data.startLine - 1, link.data.startColumn), new vscode.Position(link.data.endLine - 1, link.data.endColumn));
+						// Reveal the range
+						activeEditor.selection = new vscode.Selection(range.start, range.end);
+						activeEditor.revealRange(range);
+					}
+				});
+			},
+		}
+	);
 }
 
 // this method is called when your extension is deactivated
