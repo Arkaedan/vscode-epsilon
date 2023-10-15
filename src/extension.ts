01a4e6b6310602c6c12303dc8aa4aa86251bc5c2
Dimitris Kolovos <dimitris.kolovos@york.ac.uk>
2023-10-15 17:32:53 +0100

    Added initial support for Epsilon language server

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,46 @@
 import { TextEncoder } from 'util';
 import * as vscode from 'vscode';
 
+import * as net from 'net';
+
+import { Trace } from 'vscode-jsonrpc';
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, StreamInfo} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
+    
+    // The server is started as a separate app and listens on port 5007
+    let connectionInfo = {
+        port: 5007
+    };
+    let serverOptions = () => {
+        // Connect to language server via socket
+        let socket = net.connect(connectionInfo);
+        let result: StreamInfo = {
+            writer: socket,
+            reader: socket
+        };
+        return Promise.resolve(result);
+    };
+
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: [
+            { scheme: 'file', language: 'eol' },
+            { scheme: 'file', language: 'evl' },
+        ],
+        synchronize: {
+            fileEvents: [
+                workspace.createFileSystemWatcher('**/*.eol'),
+                workspace.createFileSystemWatcher('**/*.evl'),
+            ]
+        },
+    };
+
+    client = new LanguageClient('epsilon', 'Epsilon Editor', serverOptions, clientOptions);
+    client.setTrace(Trace.Verbose);
+    client.start();
 
 	// This line of code will only be executed once when your extension is activated
 	// console.log('Congratulations, your extension "vscode-epsilon" is now active!');
@@ -72,7 +111,12 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    if (!client) {
+        return undefined;
+      }
+      return client.stop();
+}
 
 async function createNewEgxEglPair(path: string | undefined): Promise<void> {
 	if (path === undefined) {
