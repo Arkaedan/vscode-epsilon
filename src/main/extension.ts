97bb109236bb21de44f8a7d57bc99bbde86ac3ce
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2024-04-29 13:38:21 +0100

    Add remote debugging via DAP
    This commit adds the minimal code needed to have VS Code attach to a
    running debug adapter over TCP. The initial approach is to have the Ant
    tasks start the server and wait for VS Code to attach to them before
    starting the Epsilon script to be debugged.
    
    This only requires using a DebugAdapterDescriptorFactory that looks at
    the "port" in the launch.json entry being executed, and passes that on
    to the standard DebugAdapterServer class. All the debugging complexity
    will be hidden behind the DAP server (currently being prototyped in a
    new 'debug-adapter' branch).

--- src/main/extension.ts
+++ src/main/extension.ts
@@ -3,9 +3,11 @@ import * as vscode from "vscode";
 import { registerTerminalLinkProvider } from "../common/terminal-link-provider";
 import { registerTemplateHelperCommands } from "../common/template-helpers";
 import * as lspClient from "../main/lsp-client";
+import * as debugClient from "../main/debug-client";
 
 export function activate(context: vscode.ExtensionContext) {
   lspClient.activate(context);
+  debugClient.activate(context);
   registerTerminalLinkProvider(context);
   registerTemplateHelperCommands(context);
 }
@@ -13,4 +15,5 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {
   lspClient.deactivate();
+  debugClient.deactivate();
 }
